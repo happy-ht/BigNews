a54f030ae09954e1722ab41e64263a328ba1942a
happy-ht <917298442+happy-ht@users.noreply.github.com>
2019-12-28 20:04:09 +0800

    路径封装和链接跳转

--- admin/js/https.js
+++ admin/js/https.js
@@ -14,5 +14,20 @@
                 xhr.setRequestHeader("Authorization", token);
             }
         }
-    })
+    });
+
+    //完整的url: "http://localhost:8080/api/v1/admin/user/detail"
+    // 设置请求的服务器根路径, 用于后续完整 url 的拼接
+    const baseURL = 'http://localhost:8080/api/v1';
+    //
+    const urls = {
+        // 获取用户信息
+        userInfo: baseURL + '/admin/user/info',
+        // 获取用户详情
+        userDetail: baseURL + '/admin/user/detail',
+        // 编辑用户信息
+        userEdit: baseURL + '/admin/user/edit',
+    };
+    // 把 urls 对象暴露到全局 window 对象中
+    window.urls = urls;
 })(window)
