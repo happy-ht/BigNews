efae4395b1d943fad90f36b79a3b19f772726537
happy-ht <917298442+happy-ht@users.noreply.github.com>
2019-12-28 17:41:04 +0800

    token令牌的封装优化

--- admin/js/https.js
+++ admin/js/https.js
@@ -3,14 +3,15 @@
     const token = localStorage.getItem('token');
 
     $.ajaxSetup({
+        //获取用户信息的接口
         beforeSend(xhr) {
             // 没有令牌跳转回登录页(登录接口用户用于获取令牌的)
             if (!token) {
                 location.href = './login.html';
             }
-            // 如果不是 login.html 登录页, 就同意添加请求头   不是返回-1
+            // 如果不是 login.html 登录页, 就统一添加请求头   不是返回-1
             if (location.href.indexOf('login.html') === -1) {
-                xhr.setRequestHearder("Authorization", token);
+                xhr.setRequestHeader("Authorization", token);
             }
         }
     })
